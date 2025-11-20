9cba4eee9f989c1bd9f3e2e3561e6b605bc018b3
seongjin <castlereal01@gmail.com>
2025-11-20 21:25:27 +0900

    ExecutorService로 쓰레드 풀 구현

---
+++ src/main/java/threads/latest/PrintTask.java
@@ -0,0 +1,24 @@
+package threads.latest;
+
+import java.util.concurrent.BlockingQueue;
+
+public class PrintTask implements Runnable {
+    private final BlockingQueue<String> printingQueue;
+
+    public PrintTask(BlockingQueue<String> printingQueue) {
+        this.printingQueue = printingQueue;
+    }
+
+    @Override
+    public void run() {
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                String token = printingQueue.take();
+                System.out.println(token);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                break;
+            }
+        }
+    }
+}
---
+++ src/main/java/threads/latest/WriteTask.java
@@ -0,0 +1,31 @@
+package threads.latest;
+
+import java.util.UUID;
+import java.util.concurrent.BlockingQueue;
+
+public class WriteTask implements Runnable {
+    private final BlockingQueue<String> waitingQueue;
+    private final BlockingQueue<String> printingQueue;
+
+    public WriteTask(BlockingQueue<String> waitingQueue, BlockingQueue<String> printingQueue) {
+        this.waitingQueue = waitingQueue;
+        this.printingQueue = printingQueue;
+    }
+
+    @Override
+    public void run() {
+        while(!Thread.currentThread().isInterrupted()) {
+            try{
+                String item = waitingQueue.take();
+                StringBuilder token = new StringBuilder(item);
+                String uuid = UUID.randomUUID().toString();
+                token.append("|");
+                token.append(uuid);
+                printingQueue.put(token.toString());
+            } catch (InterruptedException e){
+                Thread.currentThread().interrupt();
+                break;
+            }
+        }
+    }
+}
