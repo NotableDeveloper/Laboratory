25314febcfc4c4d0c722ca7590fbddcf208ce5e0
castlereal01 <castlereal01@gmail.com>
2026-06-04 22:48:22 +0900

    Remove jgit module: extracted to separate jgit branch

--- src/main/java/jgit/docker/init-script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# 원격 저장소 디렉터리 생성
-mkdir -p /app/data/origin
-
-# 베어 저장소 초기화
-git init --bare /app/data/origin/default.git
-
-echo "✓ Bare repository initialized at /app/data/origin/default.git"
